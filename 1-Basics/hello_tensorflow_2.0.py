f405c5bd90b1dcdeb87f33445ed25c6ca6a4e9d4
m-nasiri <mail.ai@yahoo.com>
2019-11-01 13:41:21 -0400

    init

--- 1-Basics/hello_tensorflow_2.0.py
+++ 1-Basics/hello_tensorflow_2.0.py
@@ -1,4 +1,10 @@
-""" Hellow world for tensorflow 2.0 """
+#-----------------------------------------------------------------------------#
+#coder: Majid Nasiri
+#github: https://github.com/m-nasiri/tensorflow-2.0/1-Basics/hello_tensorflow_2.0.py
+#date: 2017-December-11
+#-----------------------------------------------------------------------------#
+
+""" Hello world for tensorflow 2.0 """
 
 import tensorflow as tf
 print("tensoflow version is: %s" % tf.__version__)
